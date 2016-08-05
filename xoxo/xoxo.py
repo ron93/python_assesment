2b9b2e9059a3bd0126f26f0500b12201813d4b7b
ron93 <ronmkare1@gmail.com>
2016-08-05 14:06:19 +0300

    xoxo solution

--- xoxo/xoxo.py
+++ xoxo/xoxo.py
@@ -5,14 +5,20 @@
 # no punctuation or numbers.
 # For example: if str is "xooxxxxooxo" then the output should return false because there are 6 x's and 5 o's.
 
+#!/usr/bin/python
 
 def Xoxo(str):
     # code goes here
+    
+    sub1="x"
+    sub2 = "o"
+    str_count = str.count(sub1)
+    str_count2 = str.count(sub2)
+    
+    return str_count == str_count2
+    
 
 
-
-
-
-
+    
 # keep the function call
-print Xoxo("have fun..xoxo")
+print Xoxo("can i xxoxoxo")
